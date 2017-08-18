205d2efc5949509a9635201525a048f7cf6f5181
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure
    Change-Id: I1640d1b0bf5b109c5d90e8dda022c981d7c57bb6

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Project 404
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/404/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Project 404
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/404/sepolicy/qcom
