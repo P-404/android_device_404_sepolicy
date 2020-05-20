a9224106f3749fcd7f9ff76f78f1ffb2b23e6f78
dianlujitao <dianlujitao@lineageos.org>
2020-05-20 20:30:38 +0200

    sepolicy: Dynamically build trust policy into system/vendor
     * Introduce a new board flag TARGET_USES_PREBUILT_VENDOR_SEPOLICY and
       a sepolicy variant: dynamic
     * When TARGET_USES_PREBUILT_VENDOR_SEPOLICY=true, dynamic act as
       private policy, and vendor policy is excluded in order to avoid
       conflicts (it's not integrated to final builds anyway). When the flag
       is not set, dynamic acts as vendor policy to survive from system
       image change i.e. GSI installation.
    
    Change-Id: I8bfd078d6064616c88e2c58a9fa3aa045dddf303
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,5 +3,23 @@
 # inherit from Project 404
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/404/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/404/sepolicy/common/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/404/sepolicy/common/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
-    device/404/sepolicy/common
+    device/404/sepolicy/common/dynamic \
+    device/404/sepolicy/common/vendor
+endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,5 +3,20 @@
 # that inherit from Project 404
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/404/sepolicy/qcom/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/404/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
-    device/404/sepolicy/qcom
+    device/404/sepolicy/qcom/dynamic \
+    device/404/sepolicy/qcom/vendor
+endif
